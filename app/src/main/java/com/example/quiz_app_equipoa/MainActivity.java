62cddd0beb4c04b6a206fa2e44eed17623cb74c8
eddie-99 <edsong.arreola99@gmail.com>
2023-01-19 22:00:01 -0600

    Otro comentario en el main.

--- app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Comentario en el main activity.
+        //Otro comentario.
 
 
     }
