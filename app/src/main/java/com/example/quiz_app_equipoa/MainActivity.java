6a8b2cfdf8b1ab1bebf59de01542efdeae5d2b59
eddie-99 <edsong.arreola99@gmail.com>
2023-01-23 19:42:31 -0600

    Otro comentario en el main.

--- app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Comentario en el main activity.
         //Otro comentario.
-
-
     }
 }
