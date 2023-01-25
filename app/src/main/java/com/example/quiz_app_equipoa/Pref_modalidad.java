7178e52618d4b69cb0e24dcefb720b41ac71554e
Rocket2510 <rocket251019@gmail.com>
2023-01-25 08:46:52 -0600

    Pref_modalidad

---
+++ app/src/main/java/com/example/quiz_app_equipoa/Pref_modalidad.java
@@ -0,0 +1,14 @@
+package com.example.quiz_app_equipoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pref_modalidad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pref_modalidad);
+    }
+}
