c5fa2dfe009817c12bb0e2cc6893001035bfa337
Rocket2510 <rocket251019@gmail.com>
2023-01-24 23:06:45 -0600

    printf

--- app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
+++ app/src/main/java/com/example/quiz_app_equipoa/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Comentario en el main activity.
         //Otro comentario.
+        System.out.println("printf");
     }
 }
