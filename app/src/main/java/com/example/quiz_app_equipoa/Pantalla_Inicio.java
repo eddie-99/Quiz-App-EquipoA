3802c48ff0f47e8d5e534ac5cb0ea76fc7034760
Rocket2510 <rocket251019@gmail.com>
2023-01-25 08:57:51 -0600

    Correcion Pref_modalidad

--- app/src/main/java/com/example/quiz_app_equipoa/Pantalla_Inicio.java
+++ app/src/main/java/com/example/quiz_app_equipoa/Pantalla_Inicio.java
@@ -2,7 +2,9 @@ package com.example.quiz_app_equipoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pantalla_Inicio extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Pantalla_Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_inicio);
+    }
+
+    //Metodo para cambiar a las Preferencias de Modalidad
+    public void Modalidad (View v){
+        Intent preferencias = new Intent(this, Pref_modalidad.class);
+        startActivity(preferencias);
     }
 }
--- app/src/main/java/com/example/quiz_app_equipoa/Pref_modalidad.java
+++ app/src/main/java/com/example/quiz_app_equipoa/Pref_modalidad.java
@@ -1,14 +1,33 @@
 package com.example.quiz_app_equipoa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class Pref_modalidad extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pref_modalidad);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
     }
 }
